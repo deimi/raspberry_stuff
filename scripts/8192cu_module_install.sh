b880c565b3bac0be02cb2bb51a830f12e954b958
deimi <deimi.kbc@gmail.com>
2019-12-09 20:54:28 +0100

    fix script

--- scripts/8192cu_module_install.sh
+++ scripts/8192cu_module_install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # this script is necessery for older os images like Odroid C1 Ubuntu 18.04
 # in newer Images like latest Raspian, its not necessery
 # see http://daniel-lnx.blogspot.com/2013/01/raspberry-pi-and-isy-usb-wireless-micro.html
